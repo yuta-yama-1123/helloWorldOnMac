6d95bcd62a0b50c0a760db339037d03582c299b5
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-03 14:59:02 +0900

    Initial Commit

---
+++ helloWorldOnMac/helloWorldOnMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloWorldOnMacApp.swift
+//  helloWorldOnMac
+//
+//  Created by 山本祐太 on 2022/09/03.
+//
+
+import SwiftUI
+
+@main
+struct helloWorldOnMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
