c83f9928c42182358467150b9164ff2cfcb99727
yuta-yama-1123 <yutaforios@gmail.com>
2022-09-13 22:33:51 +0900

    インデント調整

--- helloWorldOnMac/ContentView.swift
+++ helloWorldOnMac/ContentView.swift
@@ -37,7 +37,7 @@ struct ContentView: View {
           .frame(width: 80, height: 80)
           .foregroundColor(.blue)
         }
-        .buttonStyle(PlainButtonStyle())
+          .buttonStyle(PlainButtonStyle())
         
         Text(String(intervalMinute))
           .font(
@@ -53,68 +53,65 @@ struct ContentView: View {
           intervalMinute += 1
         }) {
           Text("↑")
-          .font(
-            .system(size: 40, weight: .heavy, design: .rounded)
-          )
-          .frame(width: 80, height: 80)
-          .foregroundColor(.blue)
+            .font(
+              .system(size: 40, weight: .heavy, design: .rounded)
+            )
+            .frame(width: 80, height: 80)
+            .foregroundColor(.blue)
         }
-        .buttonStyle(PlainButtonStyle())
+          .buttonStyle(PlainButtonStyle())
       }
-      .padding(
-        EdgeInsets(top: 30, leading: 0, bottom: 15, trailing: 0)
-      )
+        .padding(
+          EdgeInsets(top: 30, leading: 0, bottom: 15, trailing: 0)
+        )
       
       Button(action: {
         if(timerController.timer == nil){
           intervalSecond = intervalMinute * 60
           timerController.start(Double(intervalSecond))
           withAnimation(.linear(duration: Double(intervalSecond))) {
-              self.value = 1.0
+            self.value = 1.0
           }
-        }else{
+        } else {
           timerController.stop()
           self.value = 0.0
         }
       }) {
         Text("\((timerController.timer != nil) ? "Stop" : "(Re)Start")")
-        .font(
-          .system(size: 50, weight: .heavy, design: .rounded)
-        )
-        .frame(width: 300, height: 100)
-        .foregroundColor(Color.white)
-        .background(.blue)
-        .cornerRadius(20)
-      }
-      .buttonStyle(PlainButtonStyle())
-          VStack {
-              Text("Remaining:")
-                  .font(
-                      .system(
-                          size: 30,
-                          weight: .heavy,
-                          design: .rounded
-                      )
-                  )
-              Text("\(timerController.countStr)")
-                  .font(
-                      .system(
-                          size: 80,
-                          weight: .heavy,
-                          design: .rounded
-                      )
-                  )
-              }
+          .font(
+            .system(size: 50, weight: .heavy, design: .rounded)
+          )
+          .frame(width: 300, height: 100)
+          .foregroundColor(Color.white)
+          .background(.blue)
+          .cornerRadius(20)
+        }
+          .buttonStyle(PlainButtonStyle())
+      
+        VStack {
+          Text("Remaining:")
+            .font(
+              .system(
+                size: 30, weight: .heavy, design: .rounded
+              )
+            )
+          Text("\(timerController.countStr)")
+            .font(
+              .system(
+                size: 80, weight: .heavy, design: .rounded
+              )
+            )
+          }
           .padding()
           VStack(alignment: .leading) {
-              Text("Get Ready...")
-                  .font(
-                      .system(
-                          size:25,
-                          weight: .heavy,
-                          design: .rounded
-                      )
-                  )
+            Text("Get Ready...")
+              .font(
+                .system(
+                  size:25,
+                  weight: .heavy,
+                  design: .rounded
+                )
+              )
             SquareProgressView(progress: $timerController.progress, fullWidth: 400)
                 .frame(width: 400, height: 20)
                 .cornerRadius(10)
